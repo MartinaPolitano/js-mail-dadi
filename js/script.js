51b0f06c0d057d3dcf26033cb684804a2ab71b10
Martina <martinapolitano23@gmail.com>
2020-08-24 16:01:24 +0200

    intestazione

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// chiedi all'utente la sua email//
+var email = prompt ("inserisci email");
+console.log
+
+
+
+// controlla che sia nella lista di chi può accedere//
+
+
+
+//stampa un messaggio appropriato sull’esito del controllo.//
