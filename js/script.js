04c4111f824033ece0cc9b05847d3f73cedc56e5
Martina <martinapolitano23@gmail.com>
2020-08-24 17:17:09 +0200

    Seconda modifica

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 // chiedi all'utente la sua email//
 var email = prompt ("inserisci email");
-console.log
-
-
+console.log(email);
 
 // controlla che sia nella lista di chi può accedere//
-
-
+var emails ["martinapolitano23@gmail.com", "pincopallino@gmail.com",
+"ciao@gmail.com"]
 
 //stampa un messaggio appropriato sull’esito del controllo.//
+
+var mail_trovata-false;
+
+for(var i=0; i<emails.lenght; i++){
+  if(email_utente == emails[i]){
+    mail_trovata = true;
+  }
+}
+
+
+
+var numeroRandomPlayer = Math.floor
